068791d2d784a1def6feff57cb3da7036c6039c2
dhruv <dhruvvarshney26@gmail.com>
2023-11-05 06:06:32 +0530

    added firebase options

---
+++ lib/app_data.dart
@@ -0,0 +1,22 @@
+import 'dart:core';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+String username = '';
+var questionData = [];
+String matchId = '';
+RxString player = ''.obs;
+RxString opponent = ''.obs;
+double score = 0.0;
+String participantId = '';
+String competitionId = '';
+String accessToken = '';
+int userId = 0;
+int competitionHour = 0;
+int competitionMinute = 0;
+int competitionSecond = 0;
+int participantLevel = 0;
+RxString round = "0".obs;
+String competitionName = 'Competition Name';
+RxBool isCompetitionWinner = false.obs;
+String baseUrl = 'http://127.0.0.1:8000//';
